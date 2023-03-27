0ad820dbe32d09cec94bf55aba3ecb461631e584
Vladislav Goncharov <vgoncharov@artec3d.com>
2023-03-27 10:37:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+
+requirements = [
+    'aiohttp',
+]
 
 setup(
-    name='azura_cast_api_client',
-    packages=find_packages(),
+    name='azura-cast-api-client',
+    packages=['azura_cast_api_client'],
+    python_requires='>=3.7',
+    requirements=requirements,
     version='0.0.1',
     description='API client for AzuraCast server',
     author='https://github.com/chloemby',
