3b271076a0d2eccd5876e134cdbe02769a6d6173
Vladislav Goncharov <vgoncharov@artec3d.com>
2023-03-27 10:50:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ requirements = [
 
 setup(
     name='azura-cast-api-client',
-    packages=find_packages(include=['azura_cast_api_client']),
+    packages=['azura_cast_api_client'],
+    include_package_data=True,
     python_requires='>=3.7',
     version='0.0.1',
     description='API client for AzuraCast server',
