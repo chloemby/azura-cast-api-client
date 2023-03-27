1182457d00882f25714dfb41ba96ce4e2d1df075
Vladislav Goncharov <vgoncharov@artec3d.com>
2023-03-27 11:25:34 +0200

    Fix /api prefix error in aiohttp client session

--- azura_cast_api_client/AzuraCastApi.py
+++ azura_cast_api_client/AzuraCastApi.py
@@ -11,15 +11,15 @@ class AzuraCastApi:
     def __init__(self, host: str, api_key: str):
         self.__host = host
         self.__api_key = api_key
-        self.__client = ClientSession(base_url=f"{host}/api", headers={"X-Auth-Key": api_key})
+        self.__client = ClientSession(base_url=host, headers={"X-Auth-Key": api_key})
 
     async def now_playing(self) -> List[dict]:
-        response = await self.__client.get('/nowplaying')
+        response = await self.__client.get('/api/nowplaying')
 
         return await response.json()
 
     async def now_playing_station(self, station_id: int) -> Dict:
-        response = await self.__client.get(f"/nowplaying/{station_id}")
+        response = await self.__client.get(f"/api/nowplaying/{station_id}")
 
         return await response.json()
 
@@ -30,7 +30,7 @@ class AzuraCastApi:
                 "file": file
             })
 
-            response = await self.__client.post(f"/station/{station_id}/file", body=body)
+            response = await self.__client.post(f"/api/station/{station_id}/file", body=body)
 
             return await response.json()
 
